2c04eae47160da4e790526f055586c0a89bd7a9e
Worlanyo Kwabla Kporfeame <kworlanyo@gmail.com>
2024-04-07 23:52:44 +0200

    Refactored all the codes in html, css and javascript files

--- index.js
+++ index.js
@@ -1,116 +1,48 @@
-const zeroBox = document.getElementById("zero-box");
-const calculation = document.getElementById("calculation");
-const clearBox = document.getElementById("clear-box");
-const total = document.getElementById("total");
-const equalBox = document.getElementById("equal-box");
-const numberBox = document.querySelectorAll(".number-box"); // this will return an array of all elements with class number-box
-const operatorBox = document.querySelectorAll(".operator-box"); // this will return an array of all elements with class operator-box
-const operatorBox1 = document.querySelectorAll(".operator-box1");
-const operatorBox2 = document.querySelector(".operator-box2");
+const calculation = document.querySelector(".calculation");
+const total = document.querySelector(".total");
+const equalBox = document.querySelector(".equal-box");
+const backspace = document.querySelector(".backspace");
+const boxes = document.querySelectorAll(".box");
 
-for (let i = 0; i < numberBox.length; i++) {
-  numberBox[i].addEventListener("click", () => {
-    // this adds an event listener to elements with class of number-box
-    calculation.innerHTML += i + 1; // this is because arrays are zero based
-    total.innerHTML = "";
-  });
-}
-
-for (let i = 0; i < operatorBox.length; i++) {
-  operatorBox[i].addEventListener("click", () => {
-    switch (i) {
-      case 0:
-        calculation.innerHTML += "+";
-        total.innerHTML = "";
-        break;
-      case 1:
-        calculation.innerHTML += "-";
-        total.innerHTML = "";
-        break;
-      case 2:
-        calculation.innerHTML += "×";
-        total.innerHTML = "";
-        break;
-      case 3:
-        calculation.innerHTML += "÷";
-        total.innerHTML = "";
-        break;
-      default:
-        break;
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (box.innerText === "C") {
+      calculation.innerText = "";
+    } else {
+      calculation.innerText += box.innerText;
     }
+    total.innerText = "";
   });
-}
-
-for (let i = 0; i < operatorBox1.length; i++) {
-  operatorBox1[i].addEventListener("click", () => {
-    switch (i) {
-      case 0:
-        calculation.innerHTML += ".";
-        total.innerHTML = "";
-        break;
-      case 1:
-        calculation.innerHTML += "(";
-        total.innerHTML = "";
-        break;
-      case 2:
-        calculation.innerHTML += ")";
-        total.innerHTML = "";
-        break;
-    }
-  });
-}
-
-zeroBox.addEventListener("click", () => {
-  calculation.innerHTML += 0;
-  total.innerHTML = "";
 });
 
-clearBox.addEventListener("click", () => {
-  calculation.innerHTML = "";
-  total.innerHTML = "";
-});
-
-operatorBox2.addEventListener("click", () => {
-  let expression = calculation.innerHTML;
-
-  // Check if the last character is an operator
-  const lastCharacter = expression.slice(-1);
-  const operators = ["+", "-", "×", "÷", ".", "(", ")"];
-
-  if (operators.includes(lastCharacter)) {
-    // If the last character is an operator, remove it
-    expression = expression.slice(0, -1);
-  } else {
-    // If the last character is not an operator, remove the last number or operator
-
-    // Find the last number or operator including the <span> tags
-    const lastElementMatch = expression.match(/<span[^>]*>[^<]*<\/span>|[^<]/g);
-    const lastElement = lastElementMatch[lastElementMatch.length - 1];
-
-    // Remove the last number or operator from the expression
-    expression = expression.slice(0, -lastElement.length);
-  }
-
-  calculation.innerHTML = expression;
+backspace.addEventListener("click", () => {
+  let expression = calculation.innerText;
+  let newExpression = expression.slice(0, -1);
+  calculation.innerText = newExpression;
 });
 
 equalBox.addEventListener("click", () => {
-  let expression = calculation.innerHTML;
+  try {
+    let expression = calculation.innerText;
 
-  // Removing the <span> elements added for color styling
-  expression = expression.replace(/<span class="add-color">/g, "");
-  expression = expression.replace(/<\/span>/g, "");
+    // Changing the operators to Javascript recognized operators
+    expression = expression.replace(/×/g, "*");
+    expression = expression.replace(/÷/g, "/");
+    expression = expression.replace(/−/g, "-");
+    expression = expression.replace(/＋/g, "+");
 
-  // Changing the operators to Javascript recognized operators
-  expression = expression.replace(/×/g, "*");
-  expression = expression.replace(/÷/g, "/");
-  expression = expression.replace(/−/g, "-");
-  expression = expression.replace(/＋/g, "+");
-
-  let calculationResult = eval(expression);
-  total.innerHTML = calculationResult;
-  calculation.innerHTML = "";
+    let calculationResult = eval(expression);
+    total.innerText = calculationResult;
+    calculation.innerText = "";
+  } catch (error) {
+    total.innerText = "Error";
+  }
 });
+
+function add(input) {
+  calculation.innerHTML += input;
+  total.innerHTML = "";
+}
 
 document.addEventListener("keydown", function (event) {
   const key = event.key;
@@ -126,89 +58,65 @@ document.addEventListener("keydown", function (event) {
     case "7":
     case "8":
     case "9":
-      calculation.innerHTML += key;
-      total.innerHTML = "";
+      add(key);
       break;
 
     case "+":
-      calculation.innerHTML += "+";
-      total.innerHTML = "";
+      add("+");
       break;
 
     case "-":
-      calculation.innerHTML += "-";
-      total.innerHTML = "";
+      add("-");
       break;
 
     case "*":
-      calculation.innerHTML += "×";
-      total.innerHTML = "";
+      add("×");
       break;
 
     case "/":
-      calculation.innerHTML += "÷";
-      total.innerHTML = "";
+      add("÷");
       break;
 
     case ".":
-      calculation.innerHTML += ".";
-      total.innerHTML = "";
+      add(".");
       break;
 
     case "(":
-      calculation.innerHTML += "(";
-      total.innerHTML = "";
+      add("(");
       break;
 
     case ")":
-      calculation.innerHTML += ")";
-      total.innerHTML = "";
+      add(")");
       break;
 
     case "Enter":
-      let expression = calculation.innerHTML;
+      try {
+        let expression = calculation.innerText;
 
-      expression = expression.replace(/×/g, "*");
-      expression = expression.replace(/÷/g, "/");
-      expression = expression.replace(/−/g, "-");
-      expression = expression.replace(/＋/g, "+");
+        // Changing the operators to Javascript recognized operators
+        expression = expression.replace(/×/g, "*");
+        expression = expression.replace(/÷/g, "/");
+        expression = expression.replace(/−/g, "-");
+        expression = expression.replace(/＋/g, "+");
 
-      let calculationResult = eval(expression);
-      total.innerHTML = calculationResult;
-      calculation.innerHTML = "";
-
-      break;
-
-    case "Escape":
-      // Clear the expression when Escape key is pressed
-      calculation.innerHTML = "";
-      total.innerHTML = "";
+        let calculationResult = eval(expression);
+        total.innerText = calculationResult;
+        calculation.innerText = "";
+      } catch (error) {
+        total.innerText = "Error";
+      }
       break;
 
     case "Backspace":
-      let expression2 = calculation.innerHTML;
+      let expression2 = calculation.innerText;
+      let newExpression = expression2.slice(0, -1);
+      calculation.innerText = newExpression;
+      break;
 
-      // Check if the last character is an operator
-      const lastCharacter = expression2.slice(-1);
-      const operators = ["+", "-", "×", "÷", ".", "(", ")"];
-
-      if (operators.includes(lastCharacter)) {
-        // If the last character is an operator, remove it
-        expression2 = expression2.slice(0, -1);
-      } else {
-        // If the last character is not an operator, remove the last number or operator
-
-        // Find the last number or operator including the <span> tags
-        const lastElementMatch = expression2.match(
-          /<span[^>]*>[^<]*<\/span>|[^<]/g
-        );
-        const lastElement = lastElementMatch[lastElementMatch.length - 1];
-
-        // Remove the last number or operator from the expression
-        expression2 = expression2.slice(0, -lastElement.length);
-      }
-
-      calculation.innerHTML = expression2;
+    case "Escape":
+      calculation.innerText = "";
+      total.innerText = "";
+      break;
 
     default:
       break;
